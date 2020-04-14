da06aa843e392f719c2620a27324c3caea09a8b3
ongzhen119 <ongzc-pm19@student.tarc.edu.my>
2020-04-14 22:02:12 +0800

    haizz problem

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('#responsive').lightSlider({
+        item:4,
+        loop:false,
+        slideMove:2,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed:600,
+        responsive : [
+            {
+                breakpoint:800,
+                settings: {
+                    item:3,
+                    slideMove:1,
+                    slideMargin:6,
+                  }
+            },
+            {
+                breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1
+                  }
+            }
+        ]
+    });  
+  });

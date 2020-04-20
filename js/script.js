52530a56a079ced65cc6cae3f5317ca18fff2b51
ongzhen119 <ongzc-pm19@student.tarc.edu.my>
2020-04-20 14:56:34 +0800

    best header footer in homepage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 //swiper script
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
+    slidesPerView: 3,
     spaceBetween: 30,
     pagination: {
       el: '.swiper-pagination',
